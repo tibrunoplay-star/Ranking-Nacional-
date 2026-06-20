32c7bc46d2836906561ae9ede164d529f4626c59
tibrunoplay-star <tibrunoplay@gmail.com>
2026-06-20 21:53:05 +0100

    Update ranking_nacional.py

--- ranking_nacional.py
+++ ranking_nacional.py
@@ -18,8 +18,8 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-print(f"Ligado como {bot.user}")
-atualizar_ranking.start()
+    print(f"Ligado como {bot.user}")
+    atualizar_ranking.start()
 
 @tasks.loop(minutes=30)
 async def atualizar_ranking():
