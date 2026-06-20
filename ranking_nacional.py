ad499d197363479d169a5466790ec24cdc238172
tibrunoplay-star <tibrunoplay@gmail.com>
2026-06-20 23:00:57 +0100

    Update ranking_nacional.py

--- ranking_nacional.py
+++ ranking_nacional.py
@@ -22,11 +22,8 @@ async def on_ready():
     atualizar_ranking.start()
 
 @tasks.loop(minutes=30)
-    async def atualizar_ranking():
-    print("Ranking nacional iniciado")
-
-@tasks.loop(minutes=30)
 async def atualizar_ranking():
+    print("Ranking nacional iniciado")
 
     canal = bot.get_channel(CANAL_ID)
 
